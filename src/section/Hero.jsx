0c46566026c8666dffcc36a28986a2c0d3e2188e
PrathamGaidhane <139666134+PrathamGaidhane@users.noreply.github.com>
2025-12-14 21:56:49 +0530

    Update developer location from Croatia to India

--- src/section/Hero.jsx
+++ src/section/Hero.jsx
@@ -51,7 +51,7 @@ const Hero = () => {
               <h1>that Deliver Results</h1>
             </div>
             <p className="text-white-50 md:text-xl relative z-10 pointer-events-none">
-              Hi, I'm Pratham, a developer based in Croatia with a passion for
+              Hi, I'm Pratham, a developer based in India with a passion for
               code.
             </p>
             <Button
